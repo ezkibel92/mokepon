d03631a4c285babe13b46b34315713bdeed68a6b
ezkibel92 <ezequielsegundo20392@gmail.com>
2022-08-01 16:31:06 -0300

    seleccionar mascota

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 
+//Este evento de window, agregando el parametro load, ejecuta el otro parametro cuando termine de cargar el HTML
+window.addEventListener('load',iniciarJuego)
+
 
 function iniciarJuego(){
 
@@ -8,9 +11,20 @@ function iniciarJuego(){
 }
 
 function seleccionarMascota(){
-    alert('SELECCIONASTE TU MASCOTA');
+    let seleccionado = '';
+    if(document.getElementById('hipodoge').checked){
+
+        seleccionado = 'Hipodoge';
+
+    }else if(document.getElementById('capipepo').checked){
+        
+        seleccionado = 'Capipepo';
+
+    }else if(document.getElementById('ratigueya').checked){
+
+        seleccionado = 'Ratigueya';
+
+    }
+    alert('SELECCIONASTE TU MASCOTA: '+seleccionado);
 }
 
-
-//Este evento de window, agregado el parametro load, ejecuta el otro parametro cuando termine de cargar el HTML
-window.addEventListener('load',iniciarJuego)
