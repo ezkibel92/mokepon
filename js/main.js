aa76b30d70d725e7ffd9a3838c08eab7af2c1459
ezkibel92 <ezequielsegundo20392@gmail.com>
2022-08-04 12:52:37 -0300

    ataques del jugador y del enemigo

--- js/main.js
+++ js/main.js
@@ -2,15 +2,71 @@
 //Este evento de window, agregando el parametro load, ejecuta el otro parametro cuando termine de cargar el HTML
 window.addEventListener('load',iniciarJuego)
 
-
+let ataqueJugador;
+let ataqueEnemigo;
 function iniciarJuego(){
 
     let botonMascota = document.getElementById('boton-mascota');
     botonMascota.addEventListener('click', seleccionarMascota);
 
+
+    let botonAgua = document.getElementById('boton-agua');
+    botonAgua.addEventListener('click', ataqueAgua);
+    botonAgua.addEventListener('click', ataqueAleatorioEnemigo);
+    let botonFuego = document.getElementById('boton-fuego');
+    botonFuego.addEventListener('click', ataqueFuego);
+    botonFuego.addEventListener('click', ataqueAleatorioEnemigo);
+    let botonTierra = document.getElementById('boton-tierra');
+    botonTierra.addEventListener('click', ataqueTierra);
+    botonTierra.addEventListener('click', ataqueAleatorioEnemigo);
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataque = aleatorio(1,3)
+
+    if (ataque == 1){
+
+        ataqueEnemigo = 'AGUA';
+        alert(ataqueEnemigo);
+
+    }else if(ataque == 2){
+
+        ataqueEnemigo = 'FUEGO';
+        alert(ataqueEnemigo);
+
+    }else if(ataque == 3){
+
+        ataqueEnemigo = 'TIERRA';
+        alert(ataqueEnemigo);
+
+    }else{
+
+        alert('ALGO SALIÓ MAL CON LOS ATAQUES DEL ENEMIGO')
+
+    }
+
+}
+
+function ataqueAgua(){
+
+    ataqueJugador = 'AGUA';
+    alert(ataqueJugador);
+}
+
+function ataqueFuego(){
+
+    ataqueJugador = 'FUEGO';
+    alert(ataqueJugador);
+}
+
+function ataqueTierra(){
+
+    ataqueJugador = 'TIERRA';
+    alert(ataqueJugador);
 }
 
 function seleccionarMascota(){
+
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
@@ -20,7 +76,7 @@ function seleccionarMascota(){
 
         alert('SELECCIONASTE TU MASCOTA: Hipodoge');
         spanMascotaJugador.innerHTML = 'Hipodoge';
-
+        
     }else if(inputCapipepo.checked){
         
         alert('SELECCIONASTE TU MASCOTA: Capipepo');
@@ -35,14 +91,14 @@ function seleccionarMascota(){
         alert('SELECCIONA UNA DE LAS MASCOTAS');
     }
 
-    seleccionarMascotaEnemigo();
-
+    
+    seleccionarMascotaEnemigo()
 }
 
 function seleccionarMascotaEnemigo(){
 
     let mascotaAleatoria = aleatorio(1,3)
-    let spanMascotaEnemigo = getElementById('mascotaEnemigo')
+    let spanMascotaEnemigo = document.getElementById('mascotaEnemigo')
 
     if(mascotaAleatoria == 1){
 
